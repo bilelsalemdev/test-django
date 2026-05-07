7df7eb42c2a799d11ba25580c5f2d99a56de8593
bilelsalemdev <bilelsalemdev@gmail.com>
2026-05-07 21:58:54 +0100

    feat: add API routing and Swagger UI
    Wire up all app URLs under /api/ prefix. Add drf-spectacular schema
    endpoint and Swagger UI at /api/docs/. Enable django-silk profiling
    in debug mode.

---
+++ config/urls.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/auth/', include('apps.accounts.urls')),
+    path('api/companies/', include('apps.companies.urls')),
+    path('api/clients/', include('apps.clients.urls')),
+    path('api/approvals/', include('apps.approvals.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
+]
+
+if settings.DEBUG:
+    urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
